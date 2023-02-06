60cb0cf6e0eedf9f267bf195e14370a89e91854c
Diego Bittencourt Mendes <mendesdiego90@gmail.com>
2023-02-06 12:57:26 +0900

    variable_names: change some variables names to be more descriptive of what they are

--- src/FinalResult.php
+++ src/FinalResult.php
@@ -1,26 +1,26 @@
 <?php
 
 class FinalResult {
-    function results($f) {
-        $d = fopen($f, "r");
-        $h = fgetcsv($d);
+    function results($filePath) {
+        $file = fopen($filePath, "r");
+        $h = fgetcsv($file);
         $rcs = [];
-        while(!feof($d)) {
-            $r = fgetcsv($d);
-            if(count($r) == 16) {
-                $amt = !$r[8] || $r[8] == "0" ? 0 : (float) $r[8];
-                $ban = !$r[6] ? "Bank account number missing" : (int) $r[6];
-                $bac = !$r[2] ? "Bank branch code missing" : $r[2];
-                $e2e = !$r[10] && !$r[11] ? "End to end id missing" : $r[10] . $r[11];
+        while(!feof($file)) {
+            $row = fgetcsv($file);
+            if(count($row) == 16) {
+                $amt = !$row[8] || $row[8] == "0" ? 0 : (float) $row[8];
+                $ban = !$row[6] ? "Bank account number missing" : (int) $row[6];
+                $bac = !$row[2] ? "Bank branch code missing" : $row[2];
+                $e2e = !$row[10] && !$row[11] ? "End to end id missing" : $row[10] . $row[11];
                 $rcd = [
                     "amount" => [
                         "currency" => $h[0],
                         "subunits" => (int) ($amt * 100)
                     ],
-                    "bank_account_name" => str_replace(" ", "_", strtolower($r[7])),
+                    "bank_account_name" => str_replace(" ", "_", strtolower($row[7])),
                     "bank_account_number" => $ban,
                     "bank_branch_code" => $bac,
-                    "bank_code" => $r[0],
+                    "bank_code" => $row[0],
                     "end_to_end_id" => $e2e,
                 ];
                 $rcs[] = $rcd;
@@ -28,8 +28,8 @@ class FinalResult {
         }
         $rcs = array_filter($rcs);
         return [
-            "filename" => basename($f),
-            "document" => $d,
+            "filename" => basename($filePath),
+            "document" => $file,
             "failure_code" => $h[1],
             "failure_message" => $h[2],
             "records" => $rcs
