55fe3016136b200728821afd5a7c52313cc2cfd1
Diego Bittencourt Mendes <mendesdiego90@gmail.com>
2023-02-07 10:16:18 +0900

    format_code: using single quotes for string literals and indent function and classes

--- src/FinalResult.php
+++ src/FinalResult.php
@@ -1,26 +1,28 @@
 <?php
 
-class FinalResult {
+class FinalResult 
+{
 
 
-    private function mapData($file, $header) {
+    private function mapData($file, $header) 
+    {
         while(!feof($file)) {
             $row = fgetcsv($file);
             if(count($row) == 16) {
-                $amount = !$row[8] || $row[8] == "0" ? 0 : (float) $row[8];
-                $accountNumber = !$row[6] ? "Bank account number missing" : (int) $row[6];
-                $branchCode = !$row[2] ? "Bank branch code missing" : $row[2];
-                $endToEndId = !$row[10] && !$row[11] ? "End to end id missing" : $row[10] . $row[11];
+                $amount = !$row[8] || $row[8] == '0' ? 0 : (float) $row[8];
+                $accountNumber = !$row[6] ? 'Bank account number missing' : (int) $row[6];
+                $branchCode = !$row[2] ? 'Bank branch code missing' : $row[2];
+                $endToEndId = !$row[10] && !$row[11] ? 'End to end id missing' : $row[10] . $row[11];
                 $record = [
-                    "amount" => [
-                        "currency" => $header[0],
-                        "subunits" => (int) ($amount * 100)
+                    'amount' => [
+                        'currency' => $header[0],
+                        'subunits' => (int) ($amount * 100)
                     ],
-                    "bank_account_name" => str_replace(" ", "_", strtolower($row[7])),
-                    "bank_account_number" => $accountNumber,
-                    "bank_branch_code" => $branchCode,
-                    "bank_code" => $row[0],
-                    "end_to_end_id" => $endToEndId,
+                    'bank_account_name' => str_replace(' ', '_', strtolower($row[7])),
+                    'bank_account_number' => $accountNumber,
+                    'bank_branch_code' => $branchCode,
+                    'bank_code' => $row[0],
+                    'end_to_end_id' => $endToEndId,
                 ];
                 $records[] = $record;
             }
@@ -31,19 +33,20 @@ class FinalResult {
 
 
 
-    public function results($filePath) {
-        $file = fopen($filePath, "r");
+    public function results($filePath) 
+    {
+        $file = fopen($filePath, 'r');
         $header = fgetcsv($file);
         $records = $this->mapData($file, $header);
 
         //if missing file or empty file, throw error or message
         
         return [
-            "filename" => basename($filePath),
-            "document" => $file,
-            "failure_code" => $header[1],
-            "failure_message" => $header[2],
-            "records" => array_filter($records)
+            'filename' => basename($filePath),
+            'document' => $file,
+            'failure_code' => $header[1],
+            'failure_message' => $header[2],
+            'records' => array_filter($records)
         ];
     }
 }
